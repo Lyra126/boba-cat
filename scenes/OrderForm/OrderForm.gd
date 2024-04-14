dcc7a168dcd0e8c733656154527d14fd75b2f024
Clarissa Cheung <55814603+lynae126@users.noreply.github.com>
2024-04-13 22:55:21 -0400

    added order

---
+++ scenes/OrderForm/OrderForm.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var boba_options = ["Regular Boba"]
+var drink_options = ["Tea", "Coffee", "Juice"]
+var syrup_options = ["25% Sugar", "50% Sugar", "75% Sugar"]
+var milk_options = ["Oat Milk", "Almond Milk", "Regular Milk"]
+var topping_options = ["Popping Boba Pearls", "Dalgona Chunks", "Fruit Jelly"]
+var order = []
+
+var prevOrderFinished = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	generate_order()
+	showOrder()	
+
+
+func generate_order():
+	var boba = boba_options[randi() % boba_options.size()]
+	var drink = drink_options[randi() % drink_options.size()]
+	var syrup = syrup_options[randi() % syrup_options.size()]
+	var milk = milk_options[randi() % milk_options.size()]
+	var topping = topping_options[randi() % topping_options.size()]
+
+	order = [boba, drink, syrup, milk, topping]
+	# Print the order (you can replace this with your order processing logic)
+	print("Customer ordered:", boba, drink, syrup, milk, "with", topping)
+	
+func showOrder():
+	var orderText = ""
+	for item in order:
+		orderText += str(item) + "\n"
+	print(orderText)
+	$Label.set_text(orderText)
--- scenes/customer_line/customer_line.gd
+++ scenes/customer_line/customer_line.gd
@@ -1,9 +1,19 @@
 extends Node2D
 
+var OrderForm
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Next.pressed.connect(self._on_next_pressed)
+	OrderForm = preload("res://scenes/OrderForm/OrderForm.tscn").instantiate()
+	add_child(OrderForm)
+	showOrder()
+
+func showOrder():
+	OrderForm.show()
+	
+func hideOrder():
+	OrderForm.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
