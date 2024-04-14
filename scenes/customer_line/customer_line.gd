6c7c8b0b4b0733e0cd2e3db79e486ffe3641e095
Clarissa Cheung <55814603+lynae126@users.noreply.github.com>
2024-04-14 11:11:47 -0400

    added waiting status bar

--- scenes/customer_line/customer_line.gd
+++ scenes/customer_line/customer_line.gd
@@ -1,15 +1,41 @@
 extends Node2D
 
+var OrderForm
+@onready var progress_bar : TextureProgressBar = $TextureProgressBar
+@onready var canfire = true
+@onready var timer = $Timer
+@onready var percentage_of_time
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Next.pressed.connect(self._on_next_pressed)
+	OrderForm = preload("res://scenes/OrderForm/OrderForm.tscn").instantiate()
+	add_child(OrderForm)
+	showOrder()
+	if canfire:
+		canfire = false
+		$Timer.start()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func showOrder():
+	OrderForm.show()
+	
+func hideOrder():
+	OrderForm.hide()
 
 
 func _on_next_pressed():
-	get_tree().change_scene_to_file("res://scenes/drink_station/liquid_station.tscn")
+	get_tree().change_scene_to_file("res://scenes/drink_station/make_boba.tscn")
+
+	
+	
+func _process(delta):
+	if timer.get_time_left() > 0:
+		percentage_of_time = ((1 - timer.get_time_left() / timer.get_wait_time()) * 100)
+		progress_bar.value = percentage_of_time
+	else:
+		progress_bar.value = 100
+
+
+func _on_timer_timeout():
+	canfire = true
+	timer.stop()
