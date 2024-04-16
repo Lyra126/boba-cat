ffda725dbbf1fb5ed1c38503fc4307fdf5680f79
Clarissa Cheung <55814603+lynae126@users.noreply.github.com>
2024-04-16 17:59:31 -0400

    stylized buttons

--- scenes/start_screen/main_scene.gd
+++ scenes/start_screen/main_scene.gd
@@ -2,12 +2,7 @@ extends Control
 
 func _ready():
 	$Play.pressed.connect(self._on_play_pressed)
-	$Quit.pressed.connect(self._on_quit_pressed)
 
 func _on_play_pressed():
 	print("play is pressed")
 	get_tree().change_scene_to_file("res://scenes/start_screen/loading_scene.tscn")
-
-func _on_quit_pressed():
-	print("quit is pressed")
-	get_tree().quit()
