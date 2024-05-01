410b6e9c7ebccfc987f24e9e484031e6364a9f4c
Clarissa Cheung <55814603+lynae126@users.noreply.github.com>
2024-05-01 19:12:37 -0400

    added credit scene:

--- scenes/start_screen/main_scene.gd
+++ scenes/start_screen/main_scene.gd
@@ -4,5 +4,8 @@ func _ready():
 	$Play.pressed.connect(self._on_play_pressed)
 
 func _on_play_pressed():
-	print("play is pressed")
 	get_tree().change_scene_to_file("res://scenes/start_screen/loading_scene.tscn")
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/start_screen/credits.tscn")
